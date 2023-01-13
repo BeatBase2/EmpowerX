a77559ac882cb3367e0cac68512392f3d0fc20aa
BeatBase2 <97335757+BeatBase2@users.noreply.github.com>
2023-01-12 21:45:48 -0500

    a

--- src/EmpowerX.java
+++ src/EmpowerX.java
@@ -1,8 +1,87 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class EmpowerX {
+    private static void printlist(ArrayList<User> UsersList){
+        for (int i = 0;i< UsersList.size();i++){
+            String user = UsersList.get(i).getUsername();
+            String pass = UsersList.get(i).getPassword();
+            System.out.println("Username: " + user + "\tPassword: " + pass);
+        }
+    }
+    private static boolean Searchlist(String name, ArrayList<User> UsersList){
+        for (int i = 0;i< UsersList.size();i++){
+            String next = UsersList.get(i).getUsername();
+
+            if (next.equalsIgnoreCase(name)){
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean Searchlist(String name,String word, ArrayList<User> UsersList){
+        for (int i = 0;i< UsersList.size();i++){
+            String user = UsersList.get(i).getUsername();
+            String pass = UsersList.get(i).getPassword();
+            if (user.equalsIgnoreCase(name)&&pass.equals(word)){
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        System.out.println("hello");
-
-
+        boolean tempboolean;
+        ArrayList<User> UsersList = new ArrayList<User>();
+        Boolean login = false;
+        String pass;
+        String username;
+        Scanner ui = new Scanner(System.in);
+        do {
+            System.out.println("1: Login 2:Create account 3: Print users");
+            int x = Integer.parseInt(ui.nextLine());
+            switch (x) {
+                case 1:
+                    tempboolean = true;
+                    do {
+                        System.out.println("Enter Username");
+                        username = ui.nextLine();
+                        System.out.println("Enter password");
+                        pass = ui.nextLine();
+                        if (Searchlist(username,pass,UsersList)) {
+                            System.out.println("Success");
+                            login = true;
+                            tempboolean = false;
+                        }
+                    }while (tempboolean);
+                    break;
+                case 2:
+                    tempboolean = true;
+                    do {
+                        do {
+                            System.out.println("Enter a Username (20 Characters max)");
+                            username = ui.nextLine();
+                            if (username.length()>20) System.out.println("Username is too long please use only 20 characters");
+                        }while (username.length()>20);
+                        if (Searchlist(username,UsersList)){
+                            System.out.println("Error, please use a username that is not in use");
+                        }else {
+                            do {
+                                System.out.println("Enter a password (20 Characters max,Case sensitive)");
+                                pass = ui.nextLine();
+                                if (pass.length()>20) System.out.println("Password is too long please use only 20 characters");
+                            }while (pass.length()>20);
+                            tempboolean = false;
+                            UsersList.add(new User(pass,username));
+                            System.out.println("Succes, Please login");
+                        }
+                    }while (tempboolean);
+                    break;
+                case 3:
+                    printlist(UsersList);
+                    break;
+            }
+        }while (login == false);
 
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,45 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class User {
+    private String password;// Minimun 8 characters max 20 characters
+    private String username;// Minimum 4 max 20 characters
+    private int usernum;//
+
+    private final int recLen = 80;
+    public User(){
+        this.setPassword("");
+        this.setUsername("");
+    }
+
+    public User(String pass,String user){
+        this.setUsername(user);
+        this.setPassword(pass);
+    }
+
+    public int getUsernum() {
+        return usernum;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsernum(int usernum) {
+        this.usernum = usernum;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void writeUser (RandomAccessFile raf, int num)throws IOException{
+        raf.seek(this.getUsernum() * recLen);
+    }
+
 }
